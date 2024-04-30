1ae16685ec607bd60cf5f7b5763428d34405d0f1
haroldcalayan <haroldcalayan@gmail.com>
2024-04-30 12:12:21 +0800

    :sparkles: Code clean-up

--- app/src/main/java/com/haroldcalayan/tamingtemper/data/source/remote/api/TamingApi.kt
+++ app/src/main/java/com/haroldcalayan/tamingtemper/data/source/remote/api/TamingApi.kt
@@ -2,12 +2,13 @@ package com.haroldcalayan.tamingtemper.data.source.remote.api
 
 import android.content.Context
 import com.google.gson.Gson
+import com.haroldcalayan.tamingtemper.BuildConfig
 import com.haroldcalayan.tamingtemper.data.source.remote.model.TamingActivityResponse
 import kotlinx.coroutines.delay
 
 class TamingApi(private val context: Context) {
     fun getTamingApi(): TamingActivityResponse {
-        val jsonString = context.assets.open("response_1709543815894.json").bufferedReader().use {it.readText()}
+        val jsonString = context.assets.open(BuildConfig.RESPONSE_JSON_FILE).bufferedReader().use {it.readText()}
         return Gson().fromJson(jsonString, TamingActivityResponse::class.java)
     }
 }
